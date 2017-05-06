36b558a209f5123781089ac2b1d54503efb59587
Serhat Şevki Dinçer <jfcgauss@gmail.com>
2017-05-06 14:22:59 +0300

    make bob pq-safe ;P

--- bob.go
+++ bob.go
@@ -5,80 +5,111 @@
 
 package bob
 
-func chi1(x *[5]uint64) { // non-linear
-	y := *x
-	x[0] ^= y[1] &^ y[4]
-	x[1] ^= y[2] &^ y[0]
-	x[2] ^= y[3] &^ y[1]
-	x[3] ^= y[4] &^ y[2]
-	x[4] ^= y[0] &^ y[3]
-}
-
-func chi2(x *[5]uint64) {
-	y := *x
-	x[0] ^= y[3] &^ y[2]
-	x[1] ^= y[4] &^ y[3]
-	x[2] ^= y[0] &^ y[4]
-	x[3] ^= y[1] &^ y[0]
-	x[4] ^= y[2] &^ y[1]
-}
-
 func rt(x, r uint64) uint64 {
 	return x<<r ^ x>>(64-r)
 }
 
-func rx1(x *[5]uint64) { // rotate & xor
+func chi1(x *[7]uint64) { // non-linear
 	y := *x
-	x[0] = y[1] ^ y[4] ^ rt(y[0], 19)
-	x[1] = y[2] ^ y[0] ^ rt(y[1], 19)
-	x[2] = y[3] ^ y[1] ^ rt(y[2], 19)
-	x[3] = y[4] ^ y[2] ^ rt(y[3], 19)
-	x[4] = y[0] ^ y[3] ^ rt(y[4], 19)
+	x[0] = y[1]&^y[6] ^ rt(y[0], 7)
+	x[1] = y[2]&^y[0] ^ rt(y[1], 7)
+	x[2] = y[3]&^y[1] ^ rt(y[2], 7)
+	x[3] = y[4]&^y[2] ^ rt(y[3], 7)
+	x[4] = y[5]&^y[3] ^ rt(y[4], 7)
+	x[5] = y[6]&^y[4] ^ rt(y[5], 7)
+	x[6] = y[0]&^y[5] ^ rt(y[6], 7)
 }
 
-func rx2(x *[5]uint64) {
+func chi2(x *[7]uint64) {
 	y := *x
-	x[0] = y[3] ^ y[2] ^ rt(y[0], 21)
-	x[1] = y[4] ^ y[3] ^ rt(y[1], 21)
-	x[2] = y[0] ^ y[4] ^ rt(y[2], 21)
-	x[3] = y[1] ^ y[0] ^ rt(y[3], 21)
-	x[4] = y[2] ^ y[1] ^ rt(y[4], 21)
+	x[0] = y[5]&^y[2] ^ rt(y[0], 19)
+	x[1] = y[6]&^y[3] ^ rt(y[1], 19)
+	x[2] = y[0]&^y[4] ^ rt(y[2], 19)
+	x[3] = y[1]&^y[5] ^ rt(y[3], 19)
+	x[4] = y[2]&^y[6] ^ rt(y[4], 19)
+	x[5] = y[3]&^y[0] ^ rt(y[5], 19)
+	x[6] = y[4]&^y[1] ^ rt(y[6], 19)
+}
+
+func chi3(x *[7]uint64) {
+	y := *x
+	x[0] = y[3]&^y[4] ^ rt(y[0], 33)
+	x[1] = y[4]&^y[5] ^ rt(y[1], 33)
+	x[2] = y[5]&^y[6] ^ rt(y[2], 33)
+	x[3] = y[6]&^y[0] ^ rt(y[3], 33)
+	x[4] = y[0]&^y[1] ^ rt(y[4], 33)
+	x[5] = y[1]&^y[2] ^ rt(y[5], 33)
+	x[6] = y[2]&^y[3] ^ rt(y[6], 33)
+}
+
+func rx1(x *[7]uint64) { // rotate & xor
+	y := *x
+	x[0] = y[1] ^ y[6] ^ rt(y[0], 57)
+	x[1] = y[2] ^ y[0] ^ rt(y[1], 57)
+	x[2] = y[3] ^ y[1] ^ rt(y[2], 57)
+	x[3] = y[4] ^ y[2] ^ rt(y[3], 57)
+	x[4] = y[5] ^ y[3] ^ rt(y[4], 57)
+	x[5] = y[6] ^ y[4] ^ rt(y[5], 57)
+	x[6] = y[0] ^ y[5] ^ rt(y[6], 57)
+}
+
+func rx2(x *[7]uint64) {
+	y := *x
+	x[0] = y[5] ^ y[2] ^ rt(y[0], 45)
+	x[1] = y[6] ^ y[3] ^ rt(y[1], 45)
+	x[2] = y[0] ^ y[4] ^ rt(y[2], 45)
+	x[3] = y[1] ^ y[5] ^ rt(y[3], 45)
+	x[4] = y[2] ^ y[6] ^ rt(y[4], 45)
+	x[5] = y[3] ^ y[0] ^ rt(y[5], 45)
+	x[6] = y[4] ^ y[1] ^ rt(y[6], 45)
+}
+
+func rx3(x *[7]uint64) {
+	y := *x
+	x[0] = y[3] ^ y[4] ^ rt(y[0], 31)
+	x[1] = y[4] ^ y[5] ^ rt(y[1], 31)
+	x[2] = y[5] ^ y[6] ^ rt(y[2], 31)
+	x[3] = y[6] ^ y[0] ^ rt(y[3], 31)
+	x[4] = y[0] ^ y[1] ^ rt(y[4], 31)
+	x[5] = y[1] ^ y[2] ^ rt(y[5], 31)
+	x[6] = y[2] ^ y[3] ^ rt(y[6], 31)
 }
 
 const c13 = 1<<52 + 1<<39 + 1<<26 + 1<<13 + 1
 
 type Bob struct {
-	x [5]uint64
+	x [7]uint64
 }
 
 func (b *Bob) perm() {
 	x := &b.x
 	chi1(x)
-	x[4] -= c13
-	rx2(x)
-	x[3] += c13
-	chi2(x)
 	x[2] -= c13
+	rx2(x)
+	x[4] += c13
+	chi3(x)
+	x[6] -= c13
 	rx1(x)
+	x[1] += c13
+	chi2(x)
+	x[3] -= c13
+	rx3(x)
+	x[5] += c13
 }
 
 func (b *Bob) Reset() {
-	for i := 0; i < 5; i++ {
+	for i := 6; i >= 0; i-- {
 		b.x[i] = 0
 	}
 }
 
-func (b *Bob) Absorb(c, d uint64) {
-	b.x[0] -= c
-	b.perm()
-	b.x[1] += d
+func (b *Bob) Absorb(i uint64) {
+	b.x[0] += i
 	b.perm()
 }
 
-func (b *Bob) Squeez() (c, d uint64) {
-	c = b.x[0]
+func (b *Bob) Squeez() uint64 {
+	o := b.x[0]
 	b.perm()
-	d = b.x[1]
-	b.perm()
-	return
+	return o
 }
--- bob_test.go
+++ bob_test.go
@@ -1,19 +1,54 @@
 package bob
 
-import "testing"
+import (
+	"sort"
+	"testing"
+)
+
+const N int = 4e8
+
+// fill ls with b's output, sort ls, cry on repeating elements
+func tsb(b *Bob, ls []uint64, t *testing.T) {
+	for i := N - 1; i >= 0; i-- {
+		ls[i] = b.Squeez()
+	}
+	sort.Slice(ls, func(i, j int) bool { return ls[i] < ls[j] })
+
+	for i := N - 1; i > 0; i-- {
+		if ls[i] == ls[i-1] {
+			t.Fatal("bad Bob!")
+		}
+	}
+}
+
+// save original Bob, seek a's output in sorted list ls, then tsb(original)
+func cmb(a *Bob, ls []uint64, t *testing.T) {
+	b := *a
+	for i := N - 1; i >= 0; i-- {
+		x := a.Squeez()
+
+		p, q := 0, N
+		for p < q {
+			m := (p + q) / 2
+			y := ls[m]
+			if x == y {
+				t.Fatal("bad Bob!")
+			}
+			if x < y {
+				q = m
+			} else {
+				p = m + 1
+			}
+		}
+	}
+
+	tsb(&b, ls, t)
+}
 
 func Test1(t *testing.T) {
-	var b Bob
-	const N = 9e7
-	mp := make(map[uint64]bool, N*2)
-
-	for i := N; i > 0; i-- {
-		c, d := b.Squeez()
-		mp[c] = true
-		mp[d] = true
-	}
-
-	if len(mp) != N*2 {
-		t.Fatal("bad Bob!", len(mp), N*2)
-	}
+	var a, b Bob
+	ls := make([]uint64, N)
+	tsb(&b, ls, t)
+	a.Absorb(1)
+	cmb(&a, ls, t)
 }
