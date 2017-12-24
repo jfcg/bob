5bfa5ae2f4021318782ced7d718e0292a73e7f93
Serhat Şevki Dinçer <jfcgauss@gmail.com>
2017-12-24 16:42:45 +0300

    make test mostly concurrent, increase N
    takes 1h12m & 14gb ddr4 on ryzen 1600 to finish

--- bob_test.go
+++ bob_test.go
@@ -5,35 +5,62 @@ import (
 	"testing"
 )
 
-const N int = 9e8
+// ls size, # of goroutines
+const N, G uint = 18e8, 8
+
+var (
+	te *testing.T
+	ls = make([]uint64, N)
+	ch = make(chan bool, G/2)
+	uc chan uint64
+)
+
+// ls[x:y] different neighbors?
+func chk(x, y uint) {
+	for i := x; i > y; i-- {
+		if ls[i] == ls[i-1] {
+			te.Fatal("bad Bob!")
+		}
+	}
+	ch <- false
+}
 
 // fill ls with b's output, sort ls, cry on repeating elements
-func tsb(b *Bob, ls []uint64, t *testing.T) {
-	for i := N - 1; i >= 0; i-- {
-		ls[i] = b.Squeez()
+func tsb(b *Bob) {
+	for i := N; i > 0; i-- {
+		ls[i-1] = b.Squeez()
 	}
 	sort.Slice(ls, func(i, j int) bool { return ls[i] < ls[j] })
 
-	for i := N - 1; i > 0; i-- {
-		if ls[i] == ls[i-1] {
-			t.Fatal("bad Bob!")
-		}
+	x, st := N-1, (N+G/2)/G // start, length of one interval
+	for i := G - 1; i > 0; i-- {
+		go chk(x, x-st)
+		x -= st
+	}
+	chk(x, 0)
+
+	// wait for friends
+	for i := G - 1; i > 0; i-- {
+		<-ch
 	}
 }
 
-// save original Bob, seek a's output in sorted list ls, then tsb(original)
-func cmb(a *Bob, ls []uint64, t *testing.T) {
-	b := *a
-	for i := N - 1; i >= 0; i-- {
-		x := a.Squeez()
+// search x in ls
+func sch() {
+	for {
+		x, ok := <-uc
+		if !ok {
+			break
+		}
 
-		p, q := 0, N
+		p, q := uint(0), N
 		for p < q {
 			m := (p + q) / 2
 			y := ls[m]
 			if x == y {
-				t.Fatal("bad Bob!")
+				te.Fatal("bad Bob!")
 			}
+
 			if x < y {
 				q = m
 			} else {
@@ -41,14 +68,35 @@ func cmb(a *Bob, ls []uint64, t *testing.T) {
 			}
 		}
 	}
-
-	tsb(&b, ls, t)
+	ch <- false
 }
 
-func Test1(t *testing.T) {
+// save original Bob, seek a's output in sorted list ls, then tsb(original)
+func cmb(a *Bob) {
+	b := *a
+	uc = make(chan uint64, G-1)
+
+	for i := G - 1; i > 0; i-- {
+		go sch()
+	}
+
+	for i := N; i > 0; i-- {
+		uc <- a.Squeez()
+	}
+	close(uc)
+
+	// wait for friends
+	for i := G - 1; i > 0; i-- {
+		<-ch
+	}
+
+	tsb(&b)
+}
+
+func Test1(x *testing.T) {
+	te = x
 	var a, b Bob
-	ls := make([]uint64, N)
-	tsb(&b, ls, t)
+	tsb(&b)
 	a.Absorb(1)
-	cmb(&a, ls, t)
+	cmb(&a)
 }
